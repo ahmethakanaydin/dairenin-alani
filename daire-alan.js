b12e73adae5b52612879a05bc7c9882376d2637b
Ahmet Aydın <ahmet@hakanaydin.me>
2021-05-04 13:43:44 +0300

    first commit

---
+++ daire-alan.js
@@ -0,0 +1,9 @@
+const arguments = process.argv.slice(2);
+
+function alan(r) {
+    let pi = Math.PI;
+    let sonuc = pi * (r*r);
+    console.log(`Yarıçapı: ${r} Dairenin Alanı: ${sonuc.toFixed(2)}`);
+};
+
+alan(arguments[0]*1);
